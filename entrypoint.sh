16616061bb7df1ec83705a2ed5c5f6eb05aaece9
Fredrik Stål <fredrik.stal@ingka.ikea.com>
2021-01-22 10:42:22 +0100

    Fixed how to get inputs in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-if [ -n "${GITHUB_API_KEY}" ]; then
-  export GITHUB_API_KEY="${GITHUB_API_KEY}"
+if [ -n "${INPUT_GITHUB_API_KEY}" ]; then
+  export GITHUB_API_KEY="${INPUT_GITHUB_API_KEY}"
 else
   echo "Input github_api_key cannot be empty"
   exit 1
 fi
 
-if [ -n "${GCP_SA_KEY}" ]; then
-  echo "${GCP_SA_KEY}" > /opt/gcp_key.json
+if [ -n "${INPUT_GCP_SA_KEY}" ]; then
+  echo "${INPUT_GCP_SA_KEY}" > /opt/gcp_key.json
   export GOOGLE_APPLICATION_CREDENTIALS=/opt/gcp_key.json
 else
   echo "Input gcp_sa_key cannot be empty"
@@ -20,10 +20,10 @@ if [ -z "${GITHUB_REPOSITORY}" ]; then
   exit 1
 fi
 
-if [ "${LOG_NAME}" != "" ]; then
-  export DEPLOY_LOG_NAME="${LOG_NAME}"
+if [ "${INPUT_LOG_NAME}" != "" ]; then
+  export DEPLOY_LOG_NAME="${INPUT_LOG_NAME}"
 fi
-export GITHUB_PR_NUMBER="${GITHUB_PULL_NUMBER}"
+export GITHUB_PR_NUMBER="${INPUT_GITHUB_PULL_NUMBER}"
 export GITHUB_REPO="${GITHUB_REPOSITORY##*/}"
 export GITHUB_REPO_OWNER="${GITHUB_REPOSITORY%%/*}"
 
